054626372930a846a4fc2e2448697d849730d955
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-07-20 23:34:30 -0300

    Cards.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Body   from "@/components/Body.tsx";
 
 export default function Home() {
     return (
-        <div className="h-dvh w-dvw overflow-x-hidden overflow-y-auto border-2 border-solid border-indigo-500">
+        <div className="h-dvh w-dvw overflow-x-hidden overflow-y-auto">
 	    <Header />
 	    <Body />
 	    <Footer />
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,20 @@ import Image from '@next/image';
 
 function Header() {
     return (
-	<div className="border-2 border-solid border-black">
+	<div className="bg-gray-200">
 	    <div className="h-full border-2 border-solid border-black">
 	        Upper header.
 	    </div>
 	    <div className="h-full">
 	        <div className="p-5 w-full flex flex-row justify-center items-center">
-	            <div className="bg-gray-500 h-10 w-1/2"></div> {/* Search bar */}
+	            <div className="flex bg-gray-300 h-10 w-1/2"> {/* Search bar */}
+	                <input className="w-full p-5" type="text" placeholder="Search for some product..."></input>
+	                <button className="flex flex-col justify-center items-center min-w-10 h-full bg-gray-400 hover:bg-gray-500">
+	                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 stroke-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                            </svg>
+	                </button>
+	            </div>
 	        </div>
 	    </div>
 	</div>
